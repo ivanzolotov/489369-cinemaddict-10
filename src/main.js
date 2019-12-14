d2a1111c16e8f837d7ff5a1f4a597c1b9477487c
ivanzolotov <ivanzolotov@ivanzolotov.ru>
2019-12-14 20:52:57 +0500

    Fix codestyle

--- src/main.js
+++ src/main.js
@@ -1,26 +1,18 @@
+import { createFilmsTemplate } from './components/films.js';
+import { createFilmTemplate } from './components/film.js';
+import { createFilmDetailsTemplate } from './components/film-details.js';
+import { createMenuTemplate } from './components/menu.js';
+import { createSortTemplate } from './components/sort.js';
+import { createShowMoreTemplate } from './components/show-more.js';
+import { createProfileRatingTemplate } from './components/profile-rating.js';
+
 const ALL_FILMS_COUNT = 5;
 const TOP_RATED_FILMS_COUNT = 2;
 const MOST_COMMENTED_FILMS_COUNT = 2;
 
-// Вам потребуются функции для получения разметки следующих компонент:
-
-// + Карточка фильма;
-// + Кнопка «Show more»;
-// + Попап(расширенная информация о фильме);
-
-import {createFilmsTemplate} from './components/films.js';
-import {createFilmTemplate} from './components/film.js';
-import {createFilmDetailsTemplate} from './components/film-details.js';
-import {createMenuTemplate} from './components/menu.js';
-import {createSortTemplate} from './components/sort.js';
-import {createShowMoreTemplate} from './components/show-more.js';
-import {createProfileRatingTemplate} from './components/profile-rating.js';
-
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
-
-// «Попап» отрисовываются 1 раз.
 
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
